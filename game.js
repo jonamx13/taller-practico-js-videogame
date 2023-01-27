ec4b53ab31e889afead4409cec9549feb74373c5
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-01-27 15:08:49 -0600

    Refactorizar funcion StartGame y evento resize

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementSize;
 
-function startGame() {
-    let canvasSize;
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
+function setCanvasSize() {
     if(window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
     } else {
@@ -15,23 +17,19 @@ function startGame() {
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    const elementSize = (canvasSize / 10) - 1;
+    elementSize = (canvasSize / 10) - 1;
 
-    console.log({ canvasSize, elementSize});
+    startGame();
+}
 
+function startGame() {
+    console.log({ canvasSize, elementSize });
+
+    /* Drawing the X emoji in the canvas. */
     game.font = elementSize + 'px Verdana';
 
     for (let i = 0; i < 10; i++) {
         game.fillText(emojis['X'], elementSize * i, elementSize);
     }
     
-    // game.fillRect(0,50,100,100);
-    // game.clearRect(50,50,50,50);
-    // game.clearRect();
-    // game.clearRect(0,0,100,50);
-
-    // game.font = '25px Verdana';
-    // game.fillStyle = 'purple'
-    // game.textAlign = 'start';
-    // game.fillText('Platzi', 25, 25);
 }
