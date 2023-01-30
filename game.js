47ad9ec2706281fe9b7e3f739803fc524fcb788c
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-01-30 13:57:14 -0600

    Refactor del mapa

--- game.js
+++ game.js
@@ -34,9 +34,18 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({ map, mapRows, mapRowCols });
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementSize * col, elementSize * row);
-        }
-    }
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementSize * (colI + 1);
+            const posY = elementSize  * (rowI + 1);
+            game.fillText(emoji, posX, posY)
+        })
+    });
+
+    // for (let row = 1; row <= 10; row++) {
+    //     for (let col = 1; col <= 10; col++) {
+    //         game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementSize * col, elementSize * row);
+    //     }
+    // }
 }
