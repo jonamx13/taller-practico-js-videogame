cfa356cfe81a1188b99f80f27111a122daa4d2e1
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-02-03 14:09:07 -0600

    Colisiones de bombas

--- game.js
+++ game.js
@@ -17,6 +17,7 @@ const giftPosition = {
     x: undefined,
     y: undefined
 }
+let enemyPositions = [];
 
 window.addEventListener('load', setCanvasSize);
 window.addEventListener('resize', setCanvasSize);
@@ -49,6 +50,7 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(''));
     console.log({ map, mapRows, mapRowCols });
 
+    enemyPositions = [];
     /* Clearing the canvas. */
     game.clearRect(0,0,canvasSize, canvasSize);
     /* Drawing the map in the canvas. */
@@ -68,6 +70,11 @@ function startGame() {
             } else if (col == 'I') {
                 giftPosition.x = posX;
                 giftPosition.y = posY;
+            } else if (col == 'X') {
+                enemyPositions.push({
+                    x: posX,
+                    y: posY,
+                })
             }
 
             game.fillText(emoji, posX, posY)
@@ -89,9 +96,20 @@ function movePlayer() {
     const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
     const giftCollision = giftCollisionX && giftCollisionY; 
 
-    if(giftCollision) {
+    if (giftCollision) {
         console.log('Subiste de nivel!');
     }
+
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(3) == playerPosition.x.toFixed(3);
+        const enemyCollisionY = enemy.y.toFixed(3) == playerPosition.y.toFixed(3);
+        return enemyCollisionX && enemyCollisionY;
+    });
+
+    if (enemyCollision) {
+        console.log('Chocaste contra un enemigo!');
+    }
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
 }
 
