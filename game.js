f1da3b7ffd03903474f6e0c268b1235fc5d6123a
[jonamx13] <[lav.jonathanom@gmail.com]>
2023-02-04 16:26:07 -0600

    Sistema de vidas

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ const btnDown = document.querySelector('#down');
 let canvasSize;
 let elementSize;
 let level = 0;
+let lives = 3;
 
 const playerPosition = {
     x: undefined,
@@ -113,7 +114,7 @@ function movePlayer() {
     });
 
     if (enemyCollision) {
-        console.log('Chocaste contra un enemigo!');
+        levelFail();
     }
 
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
@@ -122,6 +123,22 @@ function movePlayer() {
 function levelWin() {
     console.log('Subiste de nivel');
     level++;
+    startGame();
+}
+
+function levelFail() {
+    console.log('Chocaste contra un enemigo');
+    lives --;
+
+    console.log(lives);
+
+    if ( lives <= 0) {
+        level = 0;
+        lives = 3;
+    }
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
     startGame();
 }
 
